0ee244bcc1c6aff1850fe27342306535bbe3523c
Chad Crowe <ccrowe@linkedin.com>
2018-11-17 19:05:11 -0600

    getting tests to pass

--- src/test/scala/tests.scala
+++ src/test/scala/tests.scala
@@ -2,19 +2,23 @@ import collection.mutable.Stack
 import org.scalatest._
 
 class StackSpec extends FlatSpec {
-
-  "A Stack" should "pop values in last-in-first-out order" in {
-    val stack = new Stack[Int]
-    stack.push(1)
-    stack.push(2)
-    assert(stack.pop() === 2)
-    assert(stack.pop() === 1)
+  "simple assert" should "work" in {
+    assert(1 === 1)
   }
 
-  it should "throw NoSuchElementException if an empty stack is popped" in {
-    val emptyStack = new Stack[String]
-    assertThrows[NoSuchElementException] {
-      emptyStack.pop()
-    }
+  "should concat lists correctly" should "be right" in {
+    val list1 = List(1)
+    val list2 = List(2)
+    val combined = list1 ::: list2
+   assert(combined.equals(List(1,2)))
   }
+
+  "map concat" should "be correct" in {
+    var map1:Map[Int, Int] = Map(1 -> 2)
+    val map2: Map[Int, Int] = Map(3 -> 4)
+    map1 ++= map2
+    assert(map1 === Map(1 -> 2, 3 -> 4))
+
+  }
+
 }
