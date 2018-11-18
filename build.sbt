168747d6a51cf6acf2895188e3addb1a71b9473f
Chad Crowe <ccrowe@linkedin.com>
2018-11-17 19:51:24 -0600

    Added auto-formatting on save and wrote a few tests

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
 
-
+scalafmtOnCompile in ThisBuild := true
 
 libraryDependencies += "org.scalatest" % "scalatest_2.12" % "3.0.5" % "test"
--- src/main/scala/problem.scala
+++ src/main/scala/problem.scala
@@ -1,13 +1,10 @@
-
 object problem {
   val a = 5
-  def main(args: Array[String]){
+  def main(args: Array[String]) {
+    println("yes")
+    try {} catch {
 
+      case e: Exception => println(e)
+    }
   }
 }
-
-
-
-
-
-
