fd6949b8481ea23929078320775c078a9bce0869
Chad Crowe <ccrowe@linkedin.com>
2018-11-17 18:42:57 -0600

    uploading project structure

---
+++ build.sbt
@@ -0,0 +1,4 @@
+
+
+
+libraryDependencies += "org.scalatest" % "scalatest_2.12" % "3.0.5" % "test"
---
+++ src/main/scala/problem.scala
@@ -0,0 +1,13 @@
+
+object problem {
+  val a = 5
+  def main(args: Array[String]){
+
+  }
+}
+
+
+
+
+
+
