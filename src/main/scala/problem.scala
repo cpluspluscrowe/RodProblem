ca5f86bcab9de2caf55cb497df85e444faa240b2
Chad Crowe <ccrowe@linkedin.com>
2018-11-17 20:28:27 -0600

    adding some code to my test file

--- src/main/scala/problem.scala
+++ src/main/scala/problem.scala
@@ -1,10 +1,3 @@
 object problem {
-  val a = 5
-  def main(args: Array[String]) {
-    println("yes")
-    try {} catch {
-
-      case e: Exception => println(e)
-    }
-  }
+  def main(args: Array[String]) {}
 }
--- src/test/scala/tests.scala
+++ src/test/scala/tests.scala
@@ -21,33 +21,48 @@ class StackSpec extends FlatSpec {
     assert(map1 === Map(1 -> 2, 3 -> 4))
   }
 
-  def cutPieces(pieceLength: Long, cutSize: Long): List[Long] = {
+  "cut pieces" should "work" in {
+    try {
+      Pieces.cutPiece(5, 6) == List(5)
+    } catch {
+      case e: Exception => e.getMessage shouldEqual "5 was not greater than 6"
+    }
+
+    try {
+      Pieces.cutPiece(6, 6)
+    } catch {
+      case e: Exception => e.getMessage shouldEqual "6 was not greater than 6"
+    }
+    assert(Pieces.cutPiece(7, 6) == List(1, 6))
+  }
+
+  "hash list" should "work" in {
+    assert(Pieces.getPiecesHash(List(1, 2, 3)) === "1-2-3")
+  }
+
+  "cutting into pieces" should "be simple" in {
+    assert(true)
+  }
+}
+
+object Pieces {
+  def cutRod(pieces: List[Long], cutLengths: List[Long]): Unit = {
+    for (pieceLength <- pieces) {
+      for (cutSize <- cutLengths) {
+        if (pieceLength > cutSize) {
+          cutRod(pieces ::: cutPiece(pieceLength, cutSize), cutLengths)
+        }
+      }
+    }
+  }
+  def cutPiece(pieceLength: Long, cutSize: Long): List[Long] = {
     assert(pieceLength > cutSize)
     val piece1Length = pieceLength - cutSize
     val piece2Length = cutSize
     return List(piece1Length, piece2Length)
     return List(pieceLength)
   }
-
-  "cut pieces" should "work" in {
-    try {
-      cutPieces(5, 6) == List(5)
-    } catch {
-      case e: Exception => e.getMessage shouldEqual "5 was not greater than 6"
-    }
-
-    try {
-      cutPieces(6, 6)
-    } catch {
-      case e: Exception => e.getMessage shouldEqual "6 was not greater than 6"
-    }
-    assert(cutPieces(7, 6) == List(1, 6))
-  }
   def getPiecesHash(pieces: List[Long]): String = {
     pieces mkString "-"
-  }
-
-  "hash list" should "work" in {
-    assert(getPiecesHash(List(1, 2, 3)) === "1-2-3")
   }
 }
